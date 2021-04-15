7d38660a5a6592276cb52285abaa038994004d7c
Lucas Romero de Andrade <78609723+lucasrfma@users.noreply.github.com>
2021-04-15 17:32:12 -0300

    Practice 10.2

--- OracleCourse/Sources/Labs/ProductManagement/app/Shop.java
+++ OracleCourse/Sources/Labs/ProductManagement/app/Shop.java
@@ -3,6 +3,7 @@ package Labs.ProductManagement.app;
 import java.math.BigDecimal;
 // import java.util.Locale;
 import java.time.LocalDate;
+import java.util.Comparator;
 import java.util.Locale;
 
 import Labs.ProductManagement.data.*;
@@ -40,19 +41,45 @@ public class Shop
         pm.createProduct(102, "Cake", BigDecimal.valueOf(5.99),LocalDate.now().plusDays(2));
 
         /////
-        pm.changeLocale("pt-BR");
+        pm.changeLocale(LocaleSet.PT_BR);
         /////
 
         pm.printProductReport(101);
 
         pm.reviewProduct(102,5, "Perfect cake");
         pm.reviewProduct(102,4, "Very nice cake");
-        pm.reviewProduct(102,3, "Its ok...");
-        pm.reviewProduct(102,3, "Good, but too expensive");
+        pm.reviewProduct(102,5, "GREAT");
+        pm.reviewProduct(102,5, "Perfect");
         pm.printProductReport(102);
         
-        pm.printAllProductsReport();
+        
+        pm.createProduct(103, "Frapuccino", BigDecimal.valueOf(12.99));
+        
+        pm.reviewProduct(103, 3, "Good, but WAY to expensive");
+        pm.reviewProduct(103, 3, "Who can even pay for this??");
+        pm.reviewProduct(103, 4, "I quite like it");
 
-        // System.out.println(pm.findProductByID(102));
+        pm.createProduct(104, "Juice", BigDecimal.valueOf(5.99));
+        pm.reviewProduct(104, 3, "Seems industrialized, not fresh");
+        pm.createProduct(105, "Water", BigDecimal.valueOf(1.99));
+        pm.reviewProduct(105, 1, "Water should be free >:(");
+
+        
+        Comparator<Product> DescendingPriceSorter = (p1,p2) -> p2.getPrice().compareTo(p1.getPrice());
+        Comparator<Product> DescendingRatingSorter = (p1,p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
+        Comparator<Product> AscendingPriceSorter = (p1,p2) -> p1.getPrice().compareTo(p2.getPrice());
+        Comparator<Product> AscendingRatingSorter = (p1,p2) -> p1.getRating().ordinal() - p2.getRating().ordinal();
+        // unsorted
+        pm.printAllProductsReport();
+        // // Sort by price: most expensive first
+        // pm.printAllProductsReport( DescendingPriceSorter );
+        // // Sort by rating: best rating first
+        // pm.printAllProductsReport( DescendingRatingSorter );
+        
+        // Ascending Price then Ascending Rating
+        pm.printAllProductsReport(AscendingPriceSorter.thenComparing(AscendingRatingSorter));   
+        // Ascending Rating then Ascending Price
+        pm.printAllProductsReport(AscendingRatingSorter.thenComparing(AscendingPriceSorter));
+
     }
 }
